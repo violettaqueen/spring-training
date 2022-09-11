8c00d432409752814da80c12aa7a8f25145298d6
ozzy <ozzy@cydeo.com>
2022-09-11 16:08:41 -0400

    IoC

--- spring-01-Core-IoC-Bean-Annotation/src/main/java/com/cydeo/bean_annotation/ComputerTest.java
+++ spring-01-Core-IoC-Bean-Annotation/src/main/java/com/cydeo/bean_annotation/ComputerTest.java
@@ -6,16 +6,21 @@ import com.cydeo.bean_annotation.config.RandomConfig;
 import com.cydeo.bean_annotation.monitorfactory.Monitor;
 import com.cydeo.bean_annotation.monitorfactory.SonyMonitor;
 import com.cydeo.bean_annotation.motherboardfactory.Motherboard;
+import org.springframework.beans.factory.BeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class ComputerTest {
     public static void main(String[] args) {
 
+        //Creating container by using Application Context
         ApplicationContext container = new AnnotationConfigApplicationContext(ComputerConfig.class, RandomConfig.class);
 
-        Monitor theMonitor = container.getBean(Monitor.class);
-//        SonyMonitor theMonitor = container.getBean(SonyMonitor.class);
+        //Creating container by using BeanFactory
+        BeanFactory context = new AnnotationConfigApplicationContext();
+
+//        Monitor theMonitor = container.getBean(Monitor.class);
+        SonyMonitor theMonitor = container.getBean(SonyMonitor.class);
         Case theCase = container.getBean(Case.class);
         Motherboard theMotherboard = container.getBean(Motherboard.class);
 
