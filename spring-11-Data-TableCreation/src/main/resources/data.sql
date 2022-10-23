742a3a8478f35399c72db8ecdd340dc667aeaff6
Violetta <violetta.v.queen@gmail.com>
2022-10-23 10:37:28 -0600

    Spring data

---
+++ spring-11-Data-TableCreation/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into EMPLOYEES(id, name)
+values(1,'Mike');
+
+insert into student(id, first_name, last_name, email)
+values(1,'Mike', 'Smith', 'mike@cydeo.com');
+
+insert into student(id, first_name, last_name, email)
+values(2,'Tom', 'Hanks', 'tom@cydeo.com');
