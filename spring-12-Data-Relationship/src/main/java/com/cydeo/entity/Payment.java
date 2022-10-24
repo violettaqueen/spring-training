13faaf201afb4aa13a399b9c7615ad772819ea7c
Violetta <violetta.v.queen@gmail.com>
2022-10-24 09:50:35 -0600

    Spring data relationship

---
+++ spring-12-Data-Relationship/src/main/java/com/cydeo/entity/Payment.java
@@ -0,0 +1,34 @@
+package com.cydeo.entity;
+
+import com.cydeo.enums.Status;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "payments")
+@NoArgsConstructor
+@Data
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(columnDefinition = "DATE")
+    private LocalDate createdDate;
+
+    private BigDecimal amount;
+
+    @Enumerated(EnumType.STRING)
+    private Status paymentStatus;
+
+    public Payment(LocalDate createdDate, BigDecimal amount, Status paymentStatus) {
+        this.createdDate = createdDate;
+        this.amount = amount;
+        this.paymentStatus = paymentStatus;
+    }
+}
---
+++ spring-12-Data-Relationship/src/main/java/com/cydeo/enums/Status.java
@@ -0,0 +1,9 @@
+package com.cydeo.enums;
+
+public enum Status {
+
+    SUCCESS, FAILED;
+
+    Status() {
+    }
+}
