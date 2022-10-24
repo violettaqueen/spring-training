603ee606f2be91f3a7243418bdb11af8ab04a5a4
Violetta <violetta.v.queen@gmail.com>
2022-10-24 10:37:27 -0600

    Spring data relationship

--- spring-12-Data-Relationship/src/main/java/com/cydeo/entity/Payment.java
+++ spring-12-Data-Relationship/src/main/java/com/cydeo/entity/Payment.java
@@ -26,6 +26,10 @@ public class Payment {
     @Enumerated(EnumType.STRING)
     private Status paymentStatus;
 
+    @OneToOne
+    @JoinColumn(name = "payment_detail_id")
+    private PaymentDetail paymentDetail;
+
     public Payment(LocalDate createdDate, BigDecimal amount, Status paymentStatus) {
         this.createdDate = createdDate;
         this.amount = amount;
--- spring-12-Data-Relationship/src/main/java/com/cydeo/entity/PaymentDetail.java
+++ spring-12-Data-Relationship/src/main/java/com/cydeo/entity/PaymentDetail.java
@@ -23,6 +23,9 @@ public class PaymentDetail {
     @Column(columnDefinition = "DATE")
     private LocalDate payoutDate;
 
+    @OneToOne(mappedBy = "paymentDetail")
+    private Payment payment;
+
     public PaymentDetail(BigDecimal merchantPayoutAmount, BigDecimal commissionAmount, LocalDate payoutDate) {
         this.merchantPayoutAmount = merchantPayoutAmount;
         this.commissionAmount = commissionAmount;
