0ad8cab8432c53e726029a996a8d373e4c759cf3
Violetta <violetta.v.queen@gmail.com>
2023-01-26 13:19:31 -0700

    Docker

---
+++ spring-26-docker/src/main/java/com/cydeo/Spring26DockerApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring26DockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring26DockerApplication.class, args);
+	}
+
+}
